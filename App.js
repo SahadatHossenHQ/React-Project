44eee50cc48d9951f56c271ae3b011db7b6cb449
SahadatHossenHQ <freelancershahadat4@gmail.com>
2025-10-20 01:53:03 +0600

    install parcel and change manual react element to jsx

--- App.js
+++ App.js
@@ -1,11 +1,6 @@
 const root = ReactDOM.createRoot(document.querySelector(".root"));
 
-const parent = React.createElement("div", { Class: "parent" },
-    [React.createElement("div", { Class: "child1" },
-        React.createElement("h1", { Class: "h1" }, "heading from child1")),
-    React.createElement("div", { Class: "child2" },
-        React.createElement("h1", { Class: "h1" }, "heading from child2"))
-    ]);
+const parent = <h1 className="h1">heading from child1</h1>
 
 
 
